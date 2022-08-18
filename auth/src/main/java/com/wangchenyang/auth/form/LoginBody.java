3b6fccb24c7053456cc62e10fcba4d1c02cec017
wangchenyang <1581306992@qq.com>
2022-08-18 13:55:58 +0800

    登陆改为satoken模式 去掉接口文档

---
+++ auth/src/main/java/com/wangchenyang/auth/form/LoginBody.java
@@ -0,0 +1,31 @@
+package com.wangchenyang.auth.form;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 用户登录对象
+ *
+ * @author Lion Li
+ */
+@Data
+public class LoginBody {
+
+    /**
+     * 用户名
+     */
+    @NotBlank(message = "用户名不能为空")
+    @Length(min = 2, max = 20, message = "账户长度必须在2到20个字符之间")
+    private String username;
+
+    /**
+     * 用户密码
+     */
+    @NotBlank(message = "用户密码不能为空")
+    @Length(min = 5, max = 20, message = "用户密码长度必须在5到20个字符之间")
+    private String password;
+
+}
